63c3a9c20c87bd0a81f94636eb944349872d52b8
Leorah Bathula <leorahbathula@gmail.com>
2023-08-15 21:33:06 +0530

    Adding the view templates

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -1,11 +1,20 @@
 module.exports.homelist = function(req,res){
-    res.render('index',{title: 'Home'});
+    res.render('locations-list',{title: 'Home'});
+};
+/*GEt home page*/
+module.exports.homelist= function(req,res){
+    res.render('locations-list',{title: 'Home'});
 };
 
-module.exports.locationInfo = function(req,res){
-    res.render('index',{title: 'Location Info'});
+module.exports.locationInfo= function(req,res){
+    res.render('location-info',{title: 'Location info'});
 };
+
 
 module.exports.addReview = function(req,res){
-    res.render('index',{title: 'Add Review'});
+    res.render('location-review-form',{title: 'Add Review'});
+};
+
+module.exports.about = function(req,res){
+    res.render('generic-text',{title: 'About'});
 };
