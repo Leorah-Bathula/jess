fbc1ceff4284e3856d284d557f342368964756b5
Leorah Bathula <leorahbathula@gmail.com>
2023-08-15 18:18:37 +0530

    First commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('../controller/locations')
+var ctrlOthers = require('../controller/others')
+
+router.get('/',ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+router.get('/about',ctrlOthers.about);
+module.exports = router;
