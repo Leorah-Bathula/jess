76d5186ee8f97d33c0d0d8aac9cf996334714874
Leorah Bathula <leorahbathula@gmail.com>
2023-09-13 15:36:08 +0530

    mongodb commit

---
+++ app_server/model/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+
+var gracefulShutdown;
+var dbURI = "mongodb://0.0.0.0:27017/";
+if (process.env.NODE_ENV === 'production') {
+    dbURI = process.env.MONGOLAB_URI;
+}
+
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+ 
+
+
+// BRING IN YOUR SCHEMAS & MODELS
+require('./locations');
+    
